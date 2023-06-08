197a806fb79ed539b994a5078a96d887964cf04b
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-06-08 22:53:41 +0200

    add wiki route module middleware

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const wiki = require("./wiki.js");
+
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req,res)=>{
-    res.send("Hello World");
+app.use("/wiki", wiki);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Example app listenng on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Example app listenng on port ${PORT}`);
 });
